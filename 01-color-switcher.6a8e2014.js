822e38264092065820ef7729d965bff2eff00132
SmartCom80 <53076940+SmartCom80@users.noreply.github.com>
2022-08-16 13:28:48 +0000

    Deploying to gh-pages from @ SmartCom80/goit-js-hw-09@ed766b037f57a3c13ff57393df46b574a0cba631 🚀

---
+++ 01-color-switcher.6a8e2014.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");t.disabled=!0,e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,timerId=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(timerId),d.style.backgroundColor=null}));
+//# sourceMappingURL=01-color-switcher.6a8e2014.js.map
